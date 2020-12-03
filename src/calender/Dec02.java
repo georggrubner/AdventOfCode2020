65655fef56214762b6c7be004502633990070584
Georg Grubner <grubnergeorg@gmail.com>
2020-12-03 08:46:10 +0100

    solved day 2

---
+++ src/calender/Dec02.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calender;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import main.Calculation;
+
+/**
+ *
+ * @author grubn
+ */
+public class Dec02 implements Calculation {
+
+    @Override
+    public String calculateStar1(String inputFileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+        String st;
+        int result = 0;
+
+        while ((st = br.readLine()) != null) {
+            String[] s = st.split("-");
+            int min = Integer.parseInt(s[0]);
+            s = s[1].split(" ");
+            int max = Integer.parseInt(s[0]);
+            char letter = s[1].substring(0, s[1].length() - 1).charAt(0);
+            String password = s[2];
+
+            long count = password.chars().filter((value) -> value == letter).count();
+
+            if (count >= min && count <= max) {
+                result++;
+            }
+        }
+
+        return "" + result;
+    }
+
+    @Override
+    public String calculateStar2(String inputFileName) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+        String st;
+        int result = 0;
+
+        while ((st = br.readLine()) != null) {
+            String[] s = st.split("-");
+            int index1 = Integer.parseInt(s[0]) - 1;
+            s = s[1].split(" ");
+            int index2 = Integer.parseInt(s[0]) - 1;
+            char letter = s[1].substring(0, s[1].length() - 1).charAt(0);
+            String password = s[2];
+
+            char[] chars = password.toCharArray();
+
+            if ((chars[index1] == letter && chars[index2] != letter) || chars[index2] == letter && chars[index1] != letter) {
+                result++;
+            }
+        }
+
+        return "" + result;
+    }
+}
