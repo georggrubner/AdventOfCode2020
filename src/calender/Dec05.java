c4640944c470ad3befa691b2fae02c40d02633fd
Georg Grubner <grubnergeorg@gmail.com>
2020-12-05 06:47:41 +0100

    extracted duplicate code as method getSeatsSet

--- src/calender/Dec05.java
+++ src/calender/Dec05.java
@@ -2,6 +2,7 @@ package calender;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -15,32 +16,15 @@ public class Dec05 implements Calculation {
 
     @Override
     public String calculateStar1(String inputFileName) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
-        String st;
-        int maxSeatId = 0;
-
-        while ((st = br.readLine()) != null) {
-            int row = fromBinaryToInt(st.substring(0, 7));
-            int column = fromBinaryToInt(st.substring(7));
-            int seatId = row * 8 + column;
-            maxSeatId = Math.max(maxSeatId, seatId);
-        }
+        Set<Integer> seats = getSeatsSet(inputFileName);
+        int maxSeatId = seats.stream().mapToInt((value) -> value).max().getAsInt();
 
         return "" + maxSeatId;
     }
 
     @Override
     public String calculateStar2(String inputFileName) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
-        String st;
-        Set<Integer> seats = new TreeSet<>();
-
-        while ((st = br.readLine()) != null) {
-            int row = fromBinaryToInt(st.substring(0, 7));
-            int column = fromBinaryToInt(st.substring(7));
-            int seatId = row * 8 + column;
-            seats.add(seatId);
-        }
+        Set<Integer> seats = getSeatsSet(inputFileName);
 
         Iterator<Integer> it = seats.iterator();
         int previousSeatId = 0;
@@ -66,5 +50,20 @@ public class Dec05 implements Calculation {
             output = output.concat(chars[i] == 'B' || chars[i] == 'R' ? "1" : "0");
         }
         return Integer.parseInt(output, 2);
+    }
+
+    private Set<Integer> getSeatsSet(String inputFileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+        String st;
+        Set<Integer> seats = new TreeSet<>();
+
+        while ((st = br.readLine()) != null) {
+            int row = fromBinaryToInt(st.substring(0, 7));
+            int column = fromBinaryToInt(st.substring(7));
+            int seatId = row * 8 + column;
+            seats.add(seatId);
+        }
+
+        return seats;
     }
 }
